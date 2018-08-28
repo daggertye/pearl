1cb3206be17641c0c5035d73c33f96e8fe195b92
Aaron Lou <al968@cornell.edu>
2018-08-28 19:44:05 -0400

    restructuring

--- tests/vpg.py
+++ tests/vpg.py
@@ -5,14 +5,14 @@ import os
 #janky fix to path problems
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from vpg.vpg import VPG
+from agents.vpg.vpg import VPG
 from util.neural_networks import mlp
 
 def main():
     env = gym.make('CartPole-v0')
     agent = VPG(env, mlp)
 
-    agent.train(logdir='vpg_test/')
+    agent.train(logdir='tests/vpg')
     agent.run()
 
 if __name__ == '__main__' : main()
